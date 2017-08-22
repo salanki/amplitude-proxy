d11e63f3ff5c425d4ad62b659996d96ad36457b8
Peter Salanki <peter@salanki.st>
2017-08-22 00:22:41 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const Amplitude = require('amplitude');
+
+const amplitude = new Amplitude(process.env.API_TOKEN);
+
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+server.on('error', (err) => {
+  console.log(`Listener error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+  amplitude.track(JSON.parse(msg));
+  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind(process.env.LISTEN_PORT || 41255);
+
